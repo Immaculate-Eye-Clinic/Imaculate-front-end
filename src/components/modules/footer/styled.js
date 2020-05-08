04b3b162215f57f7337cbb6204ea960bf6ccedd8
madu munachi <madustanley1@gmail.com>
2020-05-08 13:50:00 +0100

    responsive footer

--- src/components/modules/footer/styled.js
+++ src/components/modules/footer/styled.js
@@ -5,21 +5,22 @@ background: url(/Asset/Img/EyeCareBackground.jpg);
 background-size: 100%;
 position: relative;
 color: white;
-`
-export const Diw = styled.div `
+
+.diw {
 background: #9370db;
 opacity: 0.8;
-`
+}
 
-export const Div = styled.div `
+.div {
 display: flex;
 justify-content: space-evenly;
 div {
     text-align: center;
     width: 45%;
 }
-`
-export const Div1 = styled.div `
+}
+
+.div1 {
 margin-left: 4em;
 margin-top: 2em;
 .name {
@@ -34,23 +35,78 @@ margin-top: 2em;
             display: inline;
         }
 }
-`
-export const Div2 = styled.div `
+ }
+
+ 
+
+ .div2 {
 margin-top: 9em;
-margin-right: 5em;
-`
-export const Div3 = styled.div `
+margin-right: 3.1em;
+
+ }
+
+.div3 {
 margin-top: 9em;
-`
-export const Dim = styled.div `
+}
+
+.dim {
 div {
     display: flex;
     justify-content: space-evenly;
     .a {
-        margin-left: 16em;
+        margin-left: 30em;
     }
     .b {
-        margin-right: 16em;
+        margin-right: 30em;
     }
 }
+}
+
+@media (max-width: 760px) {
+    .div {
+        display: flex;
+        flex-wrap: wrap;
+    }
+}
+
+@media (max-width: 760px) {
+   .div2 {
+       margin-right: 2em;
+       margin-top: 2em;
+
+   } 
+}
+
+@media (max-width: 760px) {
+ .div3 {
+       margin-top: 2em;
+ }   
+}
+
+@media (max-width: 760px) {
+  .add {
+      margin-left: 1.5em;
+  }  
+}
+
+@media (max-width: 760px) {
+ .num {
+    margin-left: 1.5em;
+    text-align: center;
+   }
+}
+
+@media (max-width: 760px) {
+ .a  {
+     margin-right: 2em;
+ }   
+}
+
+@media (max-width: 760px) {
+  .b {
+      margin-left: 2em;
+  }  
+}
 `
+
+
