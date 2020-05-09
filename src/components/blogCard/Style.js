36914458fd10cd2abbae65c3f42833cd78f7fc0f
mcbobby <madumcbobby@yahoo.com>
2020-05-09 21:42:29 +0100

    Update Style.js

--- src/components/blogCard/Style.js
+++ src/components/blogCard/Style.js
@@ -7,7 +7,7 @@ width: 350px;
 box-shadow: 0 7px 9px 0 rgba(0, 0, 0, 0.4), 0 8px 22px 0 rgba(0, 0, 0, 0.3);
 margin: 3em;
 border-radius: 1em;
-&: hover {
+:hover {
     box-shadow: 0 7px 9px 0 rgba(0, 0, 0, 0.8), 0 8px 22px 0 rgba(0, 0, 0, 0.6);
 }
 `
