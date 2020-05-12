58af5b6c52e6d0f1075337a6d8ac7dd1f5295fe0
udosinachi <udosipat@gmail.com>
2020-05-12 22:14:47 +0100

    Redesigned the blog card

--- src/components/blogCard/BlogCard.js
+++ src/components/blogCard/BlogCard.js
@@ -4,10 +4,39 @@ import {Div} from './Style'
 function BlogCard(props){
     return(
         <Div>
-            <div><img src={props.blog.imgUrl} alt="blog"/></div>
-            <div><p>{props.blog.caption}</p></div>
+            <div className='card'>
+                <div className='card-image'><img src={props.blog.imgUrl} /></div>
+
+
+                <div className='card-text'>
+                    <span className='date'>{props.blog.date}</span>
+                    <h2>{props.blog.post}</h2>
+                    <p> {props.blog.caption} </p>
+                </div>
+
+
+                <div className='card-stats'>
+                    <div className='stat'>
+                        <div className='value'> {props.blog.viewNumber} </div>
+                        <div className='type'> {props.blog.read} </div>
+                    </div>
+                    <div className='stat border'>
+                        <div className='value'> {props.blog.views} </div>
+                        <div className='type'> {props.blog.viewsText} </div>
+                    </div>
+                    <div className='stat'>
+                        <div className='value'> {props.blog.comment} </div>
+                        <div className='type'> {props.blog.commentText} </div>
+                    </div>
+                </div>
+
+
+            </div>
         </Div>
     )
 }
 
 export default BlogCard
+
+//<p>{props.blog.caption}</p>
+//<img src={props.blog.imgUrl} alt="blog"/>
--- src/components/blogCard/Style.js
+++ src/components/blogCard/Style.js
@@ -1,6 +1,90 @@
 import styled from 'styled-components'
 
 export const Div = styled.div`
+width: 24.5%;
+margin: 3em;
+align-items: center;
+.card{
+    display: grid;
+    grid-template-columns: 300px;
+    grid-template-rows: 200px 200px 60px;
+    border-radius: 18px;
+    background: white;
+    box-shadow: 5px 5px 15px rgba(0,0,0,0.9);
+    text-align: center;
+    transition: 0.5s ease;
+    cursor: pointer;
+    :hover{
+        transform: scale(1.2)
+        box-shadow: 5px 5px 15px rgba(0,0,0,0.6)
+    }
+    .card-image{
+        img{
+            width: 96.5%;
+            border-top-right-radius: 18px;
+            border-top-left-radius: 18px;
+            margin-right: 1em;
+        }   
+    }
+    .card-text{
+        margin: 25px;
+        .date{
+            color: rgb(255, 7, 110);
+            font-size: 13px;
+        }
+        p{
+            color: grey;
+            font-size: 15px;
+            font weight: 300;
+        }
+        h2{
+            margin-top: 0;
+            font-size: 28px;
+        }
+    }
+    .card-stats{
+        width: 96.5%;
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        grid-template-rows: 1fr;
+        border-bottom-left-radius: 15px;
+        border-bottom-right-radius: 15px;
+        background: rgb(255, 7, 110);
+        .stat{
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex-direction: column;
+            padding: 10px;
+            color: white;
+        }
+        .type{
+            font-size: 11px;
+            font-weight: 300;
+            text-transform: uppercase;
+        }
+        .value{
+            font-size: 22px;
+            font-weight: 500;
+        }
+        .border{
+            border-left: 1px solid rgb(172,26,87);
+            border-right: 1px solid rgb(172,26,87);
+        }
+    }
+}
+`
+
+
+
+
+
+
+
+
+
+
+{/*export const Div = styled.div`
 color: blue;
 border: 1px;
 width: 25%;
@@ -32,4 +116,4 @@ p{
     width: 35%;
     margin: 1em;
 }
-`
+`*/}
