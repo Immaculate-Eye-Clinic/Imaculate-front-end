2bc9628683c18c5f1e9294e25fd73dd13c864b3b
mcbobby <madumcbobby@yahoo.com>
2020-05-13 22:38:21 +0100

    commit

--- src/components/modules/hero/HeroStyle.js
+++ src/components/modules/hero/HeroStyle.js
@@ -9,7 +9,7 @@ export const HeroStyle = styled.div`
     height: ${props => props.height};
     background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${props => props.img});
     background-repeat: no-repeat;
-    background-size: 100%;
+    background-size: cover;
     background-position: center;
 
     .hero-text {
