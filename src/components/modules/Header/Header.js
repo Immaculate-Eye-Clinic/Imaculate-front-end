cd56734af3e592da55319ac5b03a9480ddbfc3cb
mcbobby <madumcbobby@yahoo.com>
2020-05-27 15:43:25 +0100

    fix bug

--- src/components/modules/Header/Header.js
+++ src/components/modules/Header/Header.js
@@ -1,21 +1,45 @@
 import React from 'react'
-
-import Navbar from '../navbar/navbar'
 import Hero from '../hero/Hero'
+import Toolbar from '../navbar/navbar';
+import SideDrawer from '../navbar/sideDrawer';
+import Backdrop from '../navbar/backdrop';
 
-function Header(props) {
-    return (
-        <div>
-            <Navbar 
-            bgcolor={props.bgcolor}/>
-            <Hero 
-            title={props.title}
-            subText={props.subText}
-            height={props.height}
-            img={props.img}
-            />
-        </div>
-    )
+class Header extends React.Component {
+    state = {
+        sideDrawerOpen: false,
+    };
+
+    drawerToggleClickHandler = () => {
+        this.setState(prevState => {
+            return { sideDrawerOpen: !prevState.sideDrawerOpen };
+        });
+    };
+
+    backdropClickHandler = () => {
+        this.setState({ sideDrawerOpen: false });
+    };
+
+    render() {
+        const { children } = this.props;
+        let backdrop;
+
+        if (this.state.sideDrawerOpen) {
+            backdrop = <Backdrop click={this.backdropClickHandler} />;
+        }
+        return (
+            <div>
+                <Toolbar bgcolor={this.props.bgcolor} drawerClickHandler={this.drawerToggleClickHandler} />
+                <SideDrawer show={this.state.sideDrawerOpen} />
+                {backdrop}
+                <Hero 
+                title={this.props.title}
+                subText={this.props.subText}
+                height={this.props.height}
+                img={this.props.img}
+                />
+            </div>
+        );
+    }
 }
 
-export default Header
+export default Header;
