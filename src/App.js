321a6f3e934006875ec49acd5700133138d68fff
HisRoyalWONGiness <talk2wong@gmail.com>
2020-05-28 16:45:32 +0100

    v

--- src/App.js
+++ src/App.js
@@ -3,13 +3,10 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/home/Home';
 // import store from './store';
-
-import Navbar from './components/modules/navbar/navbar'
  
 function App() {
     return (
         <BrowserRouter>
-                <Navbar />
             <Switch>
                 <Route path="/" component={Home} exact />  
             </Switch>
