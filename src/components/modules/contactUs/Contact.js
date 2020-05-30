40e64cf0c634c5b882cfa99af1ce7fc29befc791
udosinachi <udosipat@gmail.com>
2020-05-31 00:22:19 +0100

    Done with the contact design

--- src/components/modules/contactUs/Contact.js
+++ src/components/modules/contactUs/Contact.js
@@ -15,20 +15,22 @@ class Contact extends React.Component{
                     <div className='div'>
                         <h3><i>We'd love to hear from you</i></h3>
                         <h1>Contact Us</h1>
-                        <p>Do you have an enquiry? Or simply want to find out more about us?<br/> Feel free to get in touch, we'd love to hear from you!</p>
+                        <p className='p1'>Do you have an enquiry? Or simply want to find out more about us?S Feel free to get in touch, we'd love to hear from you!</p>
                         <h2 className='number'>+ 0800 000 0000</h2>
                         <h2 className='info'>+ info@immaculate.com</h2>
                         <form>
                             <h3><i>Request a call back</i></h3>
                             <input type='text' placeholder='Your Name*' className='name' /><br/><br/>
-                            <input type='email' placeholder='Email Address*' className='email' />
-                            <input type='number' placeholder='Phone Number*' className='phone' /><br/><br/>
+                            <div className='div1'>
+                                <input type='email' placeholder='Email Address*' className='email' /><br/><br/>
+                                <input type='number' placeholder='Phone Number*' className='phone' /><br/><br/>
+                            </div>
                             <input type='text' placeholder='Subject*' className='subject' /><br/><br/>
                             <textarea type='text' placeholder='Message*' /><br/>
-                            <div className='below'>
-                                <p className='p'>By submitting this form you confirm that<br/> you’re happy for us to contact you by phone,<br/> messaging and email.</p>
+                            <span className='below'>
+                                <p className='p'>By submitting this form you confirm that you’re happy for us to contact you by phone, messaging and email.</p>
                                 <button>Send Message</button>
-                            </div>
+                            </span>
                         </form>
                     </div>
                 </div>
--- src/components/modules/contactUs/ContactStyle.js
+++ src/components/modules/contactUs/ContactStyle.js
@@ -5,13 +5,28 @@ overflow: hidden;
 display: flex;
 flex-wrap: wrap-reverse;
 
+.img{
+    width: 49.5%;
+    @media (max-width: 800px){
+        width: 100%;
+    }
+    img{
+        width: 100%;
+    }
+}
+
 .body{
-    width: 700px;
-    border: 3px solid black;
+    width: 49.5%;
     background: silver;
+    box-sizing: border-box;
+    @media (max-width: 800px){
+        width: 100%;
+    }
     .div{
         margin: 60px;
-        border: 3px solid black;
+        @media (max-width: 1200px){
+            margin: 60px 20px;
+        }
         h3{
             font-size: 20px;
             letter-spacing: 3px;
@@ -20,44 +35,59 @@ flex-wrap: wrap-reverse;
         h1{
             font-size: 50px;
         }
+
         .number{
             color: #252079;
         }
 
-        .name, .subject{
-            width: 70%;
-            padding: 10px 20px;
+        input{
+            width: 90%;
+            padding: 10px 30px;
             border: none;
+            box-sizing: border-box;
+            @media (max-width: 1200px){
+                width: 100%;
+                margin: 0;
+            }
+
         }
-        .email{
-            width: 30%;
-            padding: 10px 20px;
-            border: none;
-        }
-        .phone{
-            width: 30%;
-            padding: 10px 20px;
-            border: none;
-            margin-left: 3%;
-        }
+        
         textarea{
-            width: 76%;
+            width: 90%;
             height: 200px;
+            padding: 10px 30px;
+            border: none;
+            box-sizing: border-box;
+            @media (max-width: 1200px){
+                width: 100%;
+                margin: 0;
+            }
         }
         
         .below{
-            display: flex;
+            width: 90%;
+            display: block;
+            box-sizing: border-box;
+        
             .p{
             font-size: 12px;
             }
             button{
-                padding: 18px;
+                padding: 10px 50px;
                 font-size: 14px;
                 border: none;
-                margin-left: 3%;
                 background: #252079;
                 color: white;
+                box-sizing: border-box;
                 cursor: pointer;
+                :hover{
+                    background: #252060;
+                }
+                @media (max-width: 1200px){
+                    width: 110%;
+                    padding: 10px 50px;
+                    margin: 0;
+                }
             }
         }
     }
