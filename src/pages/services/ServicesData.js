0d595bf6d49a1a7a45dbc69805b7b6ea51762743
HisRoyalWONGiness <talk2wong@gmail.com>
2020-06-03 22:04:29 +0100

    Added images to the Cards

--- src/pages/services/ServicesData.js
+++ src/pages/services/ServicesData.js
@@ -1,38 +1,38 @@
 const ServicesData = [
     {
         id: '1',
-        imgUrl:'/Asset/Img/balls.png',
+        imgUrl:'https://res.cloudinary.com/hisroyalwonginess/image/upload/v1591217990/Immaculate/New-Lady-Cat-Eye-Glasses-Frames-For-Women-Sexy-Metal-Frame-Brand-Designer-Optical-EyeGlasses-Fashion.jpg_q50_elrhvg.webp',
         caption: 'Placeholder placeholder What you must know about Corona Virus',
         readMore: 'Read More',
 
     },
     {
         id: '2',
-        imgUrl:'/Asset/Img/g.jpg',
+        imgUrl:'https://res.cloudinary.com/hisroyalwonginess/image/upload/v1591217991/Immaculate/eye-care-at-work_l338nm.jpg',
         caption: 'What you must know about Corona Virus',
         readMore: 'Read More',
     },
     {
         id: '3',
-        imgUrl:'/Asset/Img/g.jpg',
+        imgUrl:'https://res.cloudinary.com/hisroyalwonginess/image/upload/v1591217989/Immaculate/Pediatric-Ophthalmologist-vs-Optometrist-Are-All-Eye-Doctors-the-Same_wm2cxh.jpg',
         caption: 'What you must know about Corona Virus',
         readMore: 'Read More',
     },
     {
         id: '4',
-        imgUrl:'/Asset/Img/g.jpg',
+        imgUrl:'https://res.cloudinary.com/hisroyalwonginess/image/upload/v1591217989/Immaculate/EYE-CARE-for-computer-users_zsuvvg.jpg',
         caption: 'What you must know about Corona Virus',
         readMore: 'Read More',
     },
     {
         id: '5',
-        imgUrl:'/Asset/Img/g.jpg',
+        imgUrl:'https://res.cloudinary.com/hisroyalwonginess/image/upload/v1591217988/Immaculate/eyeglasses22_fkjukn.jpg',
         caption: 'What you must know about Corona Virus',
         readMore: 'Read More',
     },
     {
         id: '6',
-        imgUrl:'/Asset/Img/g.jpg',
+        imgUrl:'https://res.cloudinary.com/hisroyalwonginess/image/upload/v1591217988/Immaculate/EYE_c94ydj.jpg',
         caption: 'What you must know about Corona Virus',
         readMore: 'Read More',
     }
