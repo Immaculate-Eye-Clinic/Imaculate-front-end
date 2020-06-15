ec928359bec73ad86e8cd2e0e7c424a5413b1a82
madu munachi <madustanley1@gmail.com>
2020-06-15 16:10:56 +0100

    single blog

--- src/pages/SingleBlog/SingleBlog.js
+++ src/pages/SingleBlog/SingleBlog.js
@@ -1,24 +1,46 @@
 import React from 'react'
 import axios from 'axios'
-import {useEffect, useState} from 'react'
+import {NavLink} from 'react-router-dom'
 
 class SingleBlog extends React.Component {
-    state = {
-        data: null
-    }
 
     componentDidMount () {
-        const { param } = this.props.match.params
-        fetch(`https:/jsonplaceholder.typicode.com/posts/${param}`)
-        .then((data) => {
-            this.setState(() => ({data}))
-        })
+        const { match: {params} } = this.props;
+
+        axios.get(`https:/jsonplaceholder.typicode.com/posts/${params.postid}`)
+        .then(({ data: post }) => {
+        console.log('post', post);
+      
+        this.setState({ post });
+          });
     }
+
+    handleDelete() {
+        const { match: { params } } = this.props;
+      
+        axios.delete(`https:/jsonplaceholder.typicode.com/posts/${params.postid}`)
+          .then(() => {
+            console.log('post deleted');
+          });
+        }
+
     render() {
         return (
             <div>
-
+            <NavLink to='/blog'>
+            <div>
+                <button>Close</button>
             </div>
+            </NavLink>
+            {post.slice(startIndex, startIndex + TOTAL_PER_PAGE).map(post => (
+                <Table.Row key={post.id}>
+                  <Table.Cell>
+                    <NavLink to={`/blog/${post.id}`}>{post.title}</NavLink>
+                  </Table.Cell>
+                  <Table.Cell>{post.body}</Table.Cell>
+                </Table.Row>
+              ))}
+            </div>  
         )
     }
 }
