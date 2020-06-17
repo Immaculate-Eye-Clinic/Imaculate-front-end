a2a723a33365ce3291ef8b68f60d0e9e1d58bb24
mcbobby <madumcbobby@yahoo.com>
2020-06-17 22:42:54 +0100

    Update SingleBlog.js

--- src/pages/SingleBlog/SingleBlog.js
+++ src/pages/SingleBlog/SingleBlog.js
@@ -1,26 +1,43 @@
-import React from 'react'
+import React, { Component } from 'react';
 import axios from 'axios'
 import {NavLink} from 'react-router-dom'
 
-class SingleBlog extends React.Component {
+class SingleBlog extends Component {
 
-    constructor() {
-      super()
-      this.state = {
-        post: []
-      }
+      constructor() {
+        super();
+        this.state = {
+            post: [
+              {title: "kjdkhlkjsdh"},
+              {title: "kjdkhlkjsdh"},
+              {title: "kjdkhlkjsdh"},
+              {title: "kjdkhlkjsdh"}
+            ],
+        };
     }
 
-    componentDidMount () {
-        const { match: {params} } = this.props;
+    componentDidMount() {
+      const { params } = this.props.match;
+      axios.get(`https:/jsonplaceholder.typicode.com/posts/${params.postid}`).then(response => {
+          this.setState({ post: response.data });
+          console.log(response.data)
+      });
+  }
 
-        axios.get(`https:/jsonplaceholder.typicode.com/posts/${params.postid}`)
-        .then(({ data: post }) => {
-        console.log('post', post);
+    // componentDidMount () {
+    //     const { match: {params} } = this.props;
+
+    //     axios.get(`https:/jsonplaceholder.typicode.com/posts/${params.postid}`)
+    //     .then(response => {
+    //       // console.log(response.data)
+    //       this.setState({ post: response.data})
+    //     })
+        // .then(({ data: post }) => {
+        // console.log('post', post);
       
-        this.setState({ post });
-          });
-    }
+        // this.setState({ post: post });
+        //   });
+    // }
 
     handleDelete() {
         const { match: { params } } = this.props;
@@ -34,18 +51,32 @@ class SingleBlog extends React.Component {
     render() {
         return (
             <div>
-              <NavLink to='/blog'>
+              {/* <NavLink to='/blog'>
                   <button>Close</button>
               </NavLink>
               {this.state.post.map(blog =>{
                 return (
                   <div key={blog.id}>
-                    <h3>{blog.title.rendered}</h3>
+                    <h3>{blog.title}</h3>
                   </div>
                 )
               })
 
-              }
+              } */}
+              {this.state.post.map(single => {
+                    return (
+                        <div key={single.id}>
+                            <h1>{single.title}</h1>
+                            <h3>
+                                {single.body}
+                                
+                            </h3>
+                        </div>
+                        
+                    );
+                })}
+                              {/* {console.log(this.state.post)} */}
+
             </div>  
         )
     }
