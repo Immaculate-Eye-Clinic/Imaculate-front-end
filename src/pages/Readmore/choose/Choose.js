95228f038d2ef7198040aed21476fe42c7a4ee07
udosinachi <udosipat@gmail.com>
2020-06-21 13:27:43 +0100

    Adding media query to the choose page

--- src/pages/Readmore/choose/Choose.js
+++ src/pages/Readmore/choose/Choose.js
@@ -18,10 +18,36 @@ function Choose(){
             img={'https://res.cloudinary.com/hisroyalwonginess/image/upload/v1588437898/Immaculate/Doctors_ieplxk.jpg'}
             />
             <BlogNav />
-            <div className='texts'>
-                <div>
-                    <h1>Satisfaction Guarantee</h1>
-                    <p>We guarantee 100% satisfaction with your first consultation, or your money back. No quibble.</p>
+            <div className='body'>
+                <div className='texts'>
+                    <div className='subtext'>
+                        <h1>Satisfaction Guarantee</h1>
+                        <p>We guarantee 100% satisfaction with your first consultation, or your money back. No quibble.</p>
+                    </div><br/><hr/>
+                    <div className='subtext'>
+                        <h1>Read When You Need Us</h1>
+                        <p>We offer consultations in the evening and at weekends to fit in with busy schedules. Appointments are usually available at short notice, so you needn’t face long waits.</p>
+                    </div><br/><hr/>
+                    <div className='subtext'>
+                        <h1>Personal Services</h1>
+                        <p>We deal directly with patients to provide efficient service with the personal touch.</p>
+                    </div><br/><hr/>
+                    <div className='subtext'>
+                        <h1>Transparent Pricing</h1>
+                        <p>We publish our prices so you’re in the know about costs. It’s more affordable than you might expect.</p>
+                    </div><br/><hr/>
+                    <div className='subtext'>
+                        <h1>No Insurance Needed</h1>
+                        <p>You don’t need health insurance to use our services. Only pay for treatment when you need it. Just book an appointment online or by phone.</p>
+                    </div><br/><hr/>
+                    <div className='subtext'>
+                        <h1>Price Promise</h1>
+                        <p>We’ll match the price of any local private eye clinic for your first consultation fee. So you know you’re getting value for money.</p>
+                    </div><br/><hr/>
+                    <div className='subtext'>
+                        <h1>Transport Included</h1>
+                        <p>Let us arrange a car and driver to take you to and from home or work. And it’s on us, if you are local (within 3 miles).</p>
+                    </div>
                 </div>
             </div>
             <Contact />
--- src/pages/Readmore/choose/ChooseStyle.js
+++ src/pages/Readmore/choose/ChooseStyle.js
@@ -1,5 +1,31 @@
 import styled from 'styled-components'
 
-export const Div = styled .div`
+export const Div = styled.div`
+
+.body{
+    border: 10px solid black;
+    margin: 100px 250px;
+    .texts{
+        border: 3px solid yellow;
+        
+            @media (max-width: 1200px){
+                margin: 50px 200px;
+            }
+        .subtext{
+            border: 3px solid black;
+        width: 100%;
+        display: grid;
+        grid-template-columns: 60% 40%;
+        justify-content: space-between;
+        box-sizing: border-box;
+            h1{
+
+            }
+            p{
+
+            }
+        }
+    }
+}
 
 `
