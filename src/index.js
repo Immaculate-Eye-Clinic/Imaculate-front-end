ae8bf465040e6d9180bf5b721abcfc7ec2158038
mcbobby <madumcbobby@yahoo.com>
2020-06-23 19:19:58 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import ReactRouter from './router/Routes';
+import ReactRouter from './router/Routes.js';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
