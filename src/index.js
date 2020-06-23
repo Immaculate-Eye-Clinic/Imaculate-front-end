4aa5f0f9ca187dcfaf279c3b72a0aef3b7135014
mcbobby <madumcbobby@yahoo.com>
2020-06-23 19:23:07 +0100

    fix error

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import ReactRouter from './router/Routes.js';
+import ReactRouter from './router';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
