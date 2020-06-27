3546a533d2b744ca47ace00a5b0c69d422a0aa23
udosinachi <udosipat@gmail.com>
2020-06-27 20:28:40 +0100

    Removed readmore from navlink

--- src/pages/services/Services.js
+++ src/pages/services/Services.js
@@ -11,7 +11,7 @@ function Services (props, imgUrl){
                 <div className = 'card_text'>
                     <div className='caption'>
                         {props.service.caption}
-                        <NavLink to={props.service.readMore}><button>Read More</button></NavLink>
+                        <button>{props.service.readMore}</button>
                     </div>
                     
                 </div>
