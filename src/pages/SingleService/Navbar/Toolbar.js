733edff29cbe4cf0aa8596133664547376d187f4
madu munachi <madustanley1@gmail.com>
2020-06-28 19:58:10 +0100

    nav

--- src/pages/SingleService/Navbar/Toolbar.js
+++ src/pages/SingleService/Navbar/Toolbar.js
@@ -8,15 +8,15 @@ function Toolbar(props) {
     return(
         <Div>
             <ul className='dropdown'>
-                <li><NavLink to='/services/urgent-eye-problems' activeClassName='link' exact>Urgent Eye Problem</NavLink></li>
-                <li><NavLink to='/services/chalazion-removal' activeClassName='link'exact>Charazion Removal</NavLink></li>
-                <li><NavLink to='/services/cataract-treatment' activeClassName='link'exact>Cataract Treatment</NavLink></li>
+                <li><NavLink to='/services/urgent-eye-problems' activeClassName='link' exact><span></span>Urgent Eye Problem</NavLink></li>
+                <li><NavLink to='/services/chalazion-removal' activeClassName='link'exact><span></span>Charazion Removal</NavLink></li>
+                <li><NavLink to='/services/cataract-treatment' activeClassName='link'exact><span>Cataract</span> Treatment</NavLink></li>
                 <div>
                     <DrawerToggleButton click={props.drawerClickHandler}/>
                 </div>
-                <li><NavLink to='/services' activeClassName='link'exact>Glaucoma Treatment</NavLink></li>
-                <li><NavLink to='/services' activeClassName='link'exact>Squint Treatment</NavLink></li>
-                <li><NavLink to='/services' activeClassName='link'exact>Aqueous Shunt Surgery</NavLink></li>
+                <li><NavLink to='/services' activeClassName='link'exact><span>Glaucoma</span> Treatment</NavLink></li>
+                <li><NavLink to='/services' activeClassName='link'exact><span>Squint</span> Treatment</NavLink></li>
+                <li><NavLink to='/services' activeClassName='link'exact><span>Aqueous</span> Shunt Surgery</NavLink></li>
             </ul>
         </Div>
     )
