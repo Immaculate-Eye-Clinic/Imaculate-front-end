55e49b080973a0c5e81f9787ae17ac5759cdcf07
mcbobby <madumcbobby@yahoo.com>
2020-06-28 20:08:45 +0100

    m

--- src/components/welcomeText/WelcomeText.js
+++ src/components/welcomeText/WelcomeText.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import {Div} from './Style'
+import Slide from 'react-reveal/Slide';
 
 class WelcomeText extends React.Component{
     render(){
         return(
             <Div>
-                <div className='welcome'>
-                    <h1 className="title">Welcome to Immaculate Eye Clinic</h1>
-                    <p>Vivamus hendrerit mi non tincidunt maximus. Praesent vestibulum odio nunc, a viverra tortor vehicula ut. Fusce a sem nisl. Pellentesque maximus ipsum quis ipsum rhoncus, suscipit ornare orci dapibus. Sed
-                    Vivamus hendrerit mi non tincidunt maximus. Praesent vestibulum odio nunc, a viverra tortor vehicula ut. Fusce a sem nisl. Pellentesque maximus ipsum quis ipsum rhoncus, suscipit ornare orci dapibus. Sed</p>
-                </div>
-                <div className='image'>
-                    <img src = "/Asset/Img/hospital.svg" alt = "Hospital" />
-                </div>
+                <Slide left>
+                    <div className='welcome'>
+                        <h1 className="title">Welcome to Immaculate Eye Clinic</h1>
+                        <p>Vivamus hendrerit mi non tincidunt maximus. Praesent vestibulum odio nunc, a viverra tortor vehicula ut. Fusce a sem nisl. Pellentesque maximus ipsum quis ipsum rhoncus, suscipit ornare orci dapibus. Sed
+                        Vivamus hendrerit mi non tincidunt maximus. Praesent vestibulum odio nunc, a viverra tortor vehicula ut. Fusce a sem nisl. Pellentesque maximus ipsum quis ipsum rhoncus, suscipit ornare orci dapibus. Sed</p>
+                    </div>
+                </Slide>
+                <Slide right>
+                    <div className='image'>
+                        <img src = "/Asset/Img/hospital.svg" alt = "Hospital" />
+                    </div> 
+                </Slide>
             </Div>
         )
     }
