17eb7441d92d7850152023d2476de652e08cb138
madu munachi <madustanley1@gmail.com>
2020-07-03 21:07:51 +0100

    modal

--- src/components/modules/Modal/Modal.js
+++ src/components/modules/Modal/Modal.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import {Div} from './styled'
+import classes from '*.module.css'
 
 function Modal(props) {
     return (
         <Div>
-            <div>
+            <div 
+            className={classes.Modal}
+            style={{
+                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                opacity : props.show ? 1 : 0
+            }}
+            >
                 {props.children}
             </div>
         </Div>
