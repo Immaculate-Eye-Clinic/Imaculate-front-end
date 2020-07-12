95c221849894b19dda841e5fd50717971421c512
madu munachi <madustanley1@gmail.com>
2020-07-12 20:40:06 +0100

    profile

--- src/components/modules/Profile/Profile.js
+++ src/components/modules/Profile/Profile.js
@@ -3,7 +3,9 @@ import React from 'react'
 function Profile() {
     return (
         <div>
-            
+            <div className='image'>
+            <img src = 'https://res.cloudinary.com/hisroyalwonginess/image/upload/v1567505818/Startng/234_813_674_3132_20190810_223902_mc4hyc.jpg' alt = 'Doctor'></img>
+            </div>
         </div>
     )
 }
