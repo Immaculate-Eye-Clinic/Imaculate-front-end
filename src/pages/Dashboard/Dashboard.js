9724e1e97312301957a714431745e7c44f9ce04b
madu munachi <madustanley1@gmail.com>
2020-07-13 19:32:39 +0100

    muna

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Profile from '../../components/modules/Profile/Profile'
 
 function Dashboard() {
     return (
         <div>
-            
+            <Profile />
         </div>
     )
 }
