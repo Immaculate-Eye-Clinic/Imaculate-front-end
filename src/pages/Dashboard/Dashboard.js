d3860c0fb33fc27305aae9ad72f1ce0479885ae3
udosinachi <udosipat@gmail.com>
2020-07-28 17:22:55 +0100

    Modal is working now

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -6,26 +6,58 @@ import {Div} from './styled'
 
 import Footer from '../../components/modules/footer/footer'
 import Navbar from '../../components/modules/navbar/navbar'
-import Schedulebutton from './Schedulebutton'
+import Modal from '../../components/modules/Modal/Modal'
 
-function Dashboard() {
-    return (
-        <Div>
-            <Navbar 
-            bgcolor='gray'
-            />
-            <div className='dash'>
-                <div className='profile'>
-                    <Profile />
+
+class Dashboard extends React.Component{
+    state = {
+        modalToggle: false,
+    }
+
+    modalHandler = (e) => {
+        e.preventDefault();
+        this.setState({
+            modalToggle: !this.state.modalToggle
+        })
+    }
+    render() {
+        return (
+            <Div>
+                <Navbar 
+                bgcolor='gray'
+                />
+                <div className='dash'>
+                    <div className='profile'>
+                        <Profile />
+                    </div>
+                    <div className='tab'>
+                        <Taba />
+                    </div>
                 </div>
-                <div className='tab'>
-                    <Taba />
+                <div className='modal'>
+                    { this.state.modalToggle &&
+                        <Modal show={this.state.modalToggle} onClick={this.modalHandler} >
+                            <div>
+                                <button onClick={this.modalHandler} className='x'>X</button>
+                                <form className='form'>
+                                    <h3>Schedule</h3>
+                                    <input type='date' /><br/>
+                                    <input type='time' /><br/>
+                                    <button>Submit</button>
+                                </form>
+                            </div>
+                        </Modal>
+                    }
+                    <button 
+                        onClick={this.modalHandler} 
+                        modalClosed={this.modalHandler} 
+                        className='button'>Modal Button
+                    </button>
                 </div>
-            </div>
-            <Schedulebutton />
-            <Footer />
-        </Div>
-    )
+                <Footer />
+            </Div>
+        )
+    }
 }
 
 export default Dashboard
