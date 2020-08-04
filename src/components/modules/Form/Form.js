3199e69829da39191bfe07a7735d46759a81b4ff
madu munachi <madustanley1@gmail.com>
2020-08-04 18:12:26 +0100

    post

--- src/components/modules/Form/Form.js
+++ src/components/modules/Form/Form.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import { Editor } from 'react-draft-wysiwyg';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+
+import Header from '../Header/Header'
 
 class Form extends React.Component {
     render() {
         return (
             <div>
-                
+                <Header 
+                    bgcolor={'transparent'}
+                    title={'About Us'}
+                    subText={'Get to Know More About Us'}
+                    height={'70vh'}
+                    mobileHeight={'50vh'}
+                    img={'https://res.cloudinary.com/hisroyalwonginess/image/upload/v1588437898/Immaculate/Doctors_ieplxk.jpg'}
+                />
+                <div>
+                    <input
+                        type='file'
+                        
+                    />
+                </div>
             </div>
         )
     }
--- src/pages/Post/Post.js
+++ src/pages/Post/Post.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
+import Form from '../../components/modules/Form/Form'
+
+
 function Post() {
     return (
         <div>
-
+            <Form />
         </div>
     )
 }
