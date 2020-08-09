392913b14cec1652b8832926ef74ae826f74edfb
madu munachi <madustanley1@gmail.com>
2020-08-09 21:04:42 +0100

    muna

--- src/components/modules/Form/styled.js
+++ src/components/modules/Form/styled.js
@@ -1,5 +1,12 @@
 import styled from 'styled-components'
 
 export const Post = styled.div `
-
+background: whitesmoke;
+.main {
+    width: 80%;
+    text-align: center;
+    position: relative;
+    left: 10%;
+    background: white;
+}
 `
